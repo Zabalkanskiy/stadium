b303f9d7d147327e85af9454aebfeb6e8dce25f5
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-08-22 12:09:17 +0300

    Change name application add new firebase

--- app/src/androidTest/java/com/example/stadium/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/stadium/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.scaffold
+package com.example.stadium
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/test/java/com/example/stadium/ExampleUnitTest.kt
+++ app/src/test/java/com/example/stadium/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.scaffold
+package com.example.stadium
 
 import org.junit.Test
 
